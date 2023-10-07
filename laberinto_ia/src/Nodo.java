445dbf278b248311999b8338ea3f9cc9870a541b
David Rodriguez <david.rodriguez03@alumnos.ucn.cl>
2023-10-07 20:04:02 -0300

    Desarrollo de algoritmo a*

--- laberinto_ia/src/Nodo.java
+++ laberinto_ia/src/Nodo.java
@@ -2,10 +2,28 @@ import java.awt.Point;
 
 public class Nodo {
     Point nodo;
+    Nodo padre;
     int f;
 
-    public Nodo(Point nodo, int f) {
+    public Nodo(Point nodo) {
         this.nodo = nodo;
+    }
+    public int getF() {
+        return f;
+    }
+    public Point getNodo() {
+        return nodo;
+    }
+    public Nodo getPadre() {
+        return padre;
+    }
+    public void setF(int f) {
         this.f = f;
+    }
+    public void setNodo(Point nodo) {
+        this.nodo = nodo;
+    }
+    public void setPadre(Nodo padre) {
+        this.padre = padre;
     }
 }
