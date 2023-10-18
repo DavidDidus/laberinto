e7376eeb28f47f1b026324c8f68aab2063f4e12b
David Rodriguez <david.rodriguez03@alumnos.ucn.cl>
2023-10-18 14:59:43 -0300

    Comentarios finales

--- laberinto_ia/src/App.java
+++ laberinto_ia/src/App.java
@@ -7,7 +7,15 @@
  * 
  * ======================================
  * 
- * En un principio el laberinto comienza vacio, al hacer click
+ * En un principio el laberinto comienza vacio, al hacer click se inserta una pared en el laberinto,
+ * si presiona pared esta se quita, para poner el punto de inicio hay que presionar el boton que dice 
+ * "seleccionar punto de partida " y luego presionar una casilla donde se quiera poner el punto,
+ * si quiere cambiar el punto solo tiene que apretar nuevamente el boton y presionar la casilla deseada
+ * para poner el punto de meta funciona igual, se presiona el boton "seleccionar punto de meta" y luego se
+ * presiona la casilla deseada.
+ * Luego de tener el laberinto deseado se presiona el boton "iniciar algoritmo " para que el algoritmo inicie
+ * y el camino se dibuje, luego de dibujado no se podra volver a interactuar con el laberinto hasta que se
+ * presione el boton "limpiar".
  * 
  */
 
